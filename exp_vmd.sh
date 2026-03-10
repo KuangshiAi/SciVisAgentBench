c5248b6f318d6e47ea83a93a08b76cd773397aef
KuangshiAi <554823301@qq.com>
2026-03-10 16:15:28 -0400

    update gitignore

--- exp_vmd.sh
+++ exp_vmd.sh
@@ -5,7 +5,7 @@ python -m benchmark.evaluation_framework.run_evaluation \
     --yaml benchmark/eval_cases/molecular_vis/eval_analysis_tasks.yaml \
     --cases SciVisAgentBench-tasks/molecular_vis \
     --eval-model gpt-5.2 \
-    --experiment-number exp1
+    --experiment-number exp3
 
 # Run cases in benchmark/eval_cases/molecular_vis/eval_analysis_workflows.yaml
 python -m benchmark.evaluation_framework.run_evaluation \
@@ -14,7 +14,7 @@ python -m benchmark.evaluation_framework.run_evaluation \
     --yaml benchmark/eval_cases/molecular_vis/eval_analysis_workflows.yaml \
     --cases SciVisAgentBench-tasks/molecular_vis \
     --eval-model gpt-5.2 \
-    --experiment-number exp1 \
+    --experiment-number exp3 \
     --exe-only
 
 python -m benchmark.evaluation_framework.run_evaluation \
@@ -23,7 +23,7 @@ python -m benchmark.evaluation_framework.run_evaluation \
     --yaml benchmark/eval_cases/molecular_vis/eval_analysis_workflows.yaml \
     --cases SciVisAgentBench-tasks/molecular_vis \
     --eval-model gpt-5.2 \
-    --experiment-number exp1 \
+    --experiment-number exp3 \
     --eval-only
 
 # Generate report for all cases (which includes both eval_analysis_tasks.yaml and eval_analysis_workflows.yaml)
@@ -32,7 +32,7 @@ python -m benchmark.evaluation_reporter.run_reporter \
     --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
     --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
     --cases SciVisAgentBench-tasks/molecular_vis \
-    --test-results test_results/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp1 \
-    --output eval_reports/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp1 \
-    --agent-mode gmx_vmd_mcp_gpt-5.2_exp1 \
+    --test-results test_results/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp3 \
+    --output eval_reports/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp3 \
+    --agent-mode gmx_vmd_mcp_gpt-5.2_exp3 \
     --port 8081
