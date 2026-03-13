065bce0f29f97c35c654cac3ae84e79adf5bd06f
KuangshiAi <554823301@qq.com>
2026-03-13 16:15:30 -0400

    fixed napari_mcp

--- exp_bioimage.sh
+++ exp_bioimage.sh
@@ -11,7 +11,91 @@ python -m benchmark.evaluation_reporter.run_reporter \
     --config benchmark/configs/napari_mcp/config_openai.json \
     --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
     --cases SciVisAgentBench-tasks/bioimage_data/eval_visualization_tasks \
+    --test-results test_results/bioimage_data/napari_mcp_gpt-5.2_exp1 \
+    --output eval_reports_claude-opus-4-6/bioimage/napari_mcp_gpt-5.2_exp1 \
+    --agent-mode napari_mcp_gpt-5.2_exp1
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp2
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data/eval_visualization_tasks \
+    --test-results test_results/bioimage_data/napari_mcp_gpt-5.2_exp2 \
+    --output eval_reports_claude-opus-4-6/bioimage/napari_mcp_gpt-5.2_exp2 \
+    --agent-mode napari_mcp_gpt-5.2_exp2
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data \
+    --eval-model claude-opus-4-6
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data/eval_visualization_tasks \
     --test-results test_results/bioimage_data/napari_mcp_gpt-5.2_exp3 \
     --output eval_reports_claude-opus-4-6/bioimage/napari_mcp_gpt-5.2_exp3 \
-    --agent-mode napari_mcp_gpt-5.2_exp3 \
-    --port 8081
+    --agent-mode napari_mcp_gpt-5.2_exp3
+
+
+# Sonnet as backbone
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp1
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data/eval_visualization_tasks \
+    --test-results test_results/bioimage_data/napari_mcp_claude-sonnet-4-5_exp1 \
+    --output eval_reports_claude-opus-4-6/bioimage/napari_mcp_claude-sonnet-4-5_exp1 \
+    --agent-mode napari_mcp_claude-sonnet-4-5_exp1
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp2
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data/eval_visualization_tasks \
+    --test-results test_results/bioimage_data/napari_mcp_claude-sonnet-4-5_exp2 \
+    --output eval_reports_claude-opus-4-6/bioimage/napari_mcp_claude-sonnet-4-5_exp2 \
+    --agent-mode napari_mcp_claude-sonnet-4-5_exp2
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data \
+    --eval-model claude-opus-4-6
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent napari_mcp \
+    --config benchmark/configs/napari_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
+    --cases SciVisAgentBench-tasks/bioimage_data/eval_visualization_tasks \
+    --test-results test_results/bioimage_data/napari_mcp_claude-sonnet-4-5_exp3 \
+    --output eval_reports_claude-opus-4-6/bioimage/napari_mcp_claude-sonnet-4-5_exp3 \
+    --agent-mode napari_mcp_claude-sonnet-4-5_exp3
--- exp_vmd.sh
+++ exp_vmd.sh
@@ -1,38 +1,103 @@
-# Run cases in benchmark/eval_cases/molecular_vis/eval_analysis_tasks.yaml 
+# GPT-5.2 backbone
 python -m benchmark.evaluation_framework.run_evaluation \
     --agent gmx_vmd_mcp \
     --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
-    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_tasks.yaml \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
     --cases SciVisAgentBench-tasks/molecular_vis \
-    --eval-model gpt-5.2 \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp1
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent gmx_vmd_mcp_agent \
+    --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --test-results test_results/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp1 \
+    --output eval_reports_claude-opus-4-6/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp1 \
+    --agent-mode gmx_vmd_mcp_gpt-5.2_exp1
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent gmx_vmd_mcp \
+    --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp2
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent gmx_vmd_mcp_agent \
+    --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --test-results test_results/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp2 \
+    --output eval_reports_claude-opus-4-6/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp2 \
+    --agent-mode gmx_vmd_mcp_gpt-5.2_exp2
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent gmx_vmd_mcp \
+    --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --eval-model claude-opus-4-6 \
     --experiment-number exp3
 
-# Run cases in benchmark/eval_cases/molecular_vis/eval_analysis_workflows.yaml
-python -m benchmark.evaluation_framework.run_evaluation \
-    --agent gmx_vmd_mcp \
-    --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
-    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_workflows.yaml \
-    --cases SciVisAgentBench-tasks/molecular_vis \
-    --eval-model gpt-5.2 \
-    --experiment-number exp3 \
-    --exe-only
-
-python -m benchmark.evaluation_framework.run_evaluation \
-    --agent gmx_vmd_mcp \
-    --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
-    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_workflows.yaml \
-    --cases SciVisAgentBench-tasks/molecular_vis \
-    --eval-model gpt-5.2 \
-    --experiment-number exp3 \
-    --eval-only
-
-# Generate report for all cases (which includes both eval_analysis_tasks.yaml and eval_analysis_workflows.yaml)
 python -m benchmark.evaluation_reporter.run_reporter \
     --agent gmx_vmd_mcp_agent \
     --config benchmark/configs/gmx_vmd_mcp/config_openai.json \
     --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
     --cases SciVisAgentBench-tasks/molecular_vis \
     --test-results test_results/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp3 \
-    --output eval_reports/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp3 \
-    --agent-mode gmx_vmd_mcp_gpt-5.2_exp3 \
-    --port 8081
+    --output eval_reports_claude-opus-4-6/molecular_vis/gmx_vmd_mcp_gpt-5.2_exp3 \
+    --agent-mode gmx_vmd_mcp_gpt-5.2_exp3
+
+# Sonnet as backbone
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent gmx_vmd_mcp \
+    --config benchmark/configs/gmx_vmd_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp1
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent gmx_vmd_mcp_agent \
+    --config benchmark/configs/gmx_vmd_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --test-results test_results/molecular_vis/gmx_vmd_mcp_claude-sonnet-4-5_exp1 \
+    --output eval_reports_claude-opus-4-6/molecular_vis/gmx_vmd_mcp_claude-sonnet-4-5_exp1 \
+    --agent-mode gmx_vmd_mcp_claude-sonnet-4-5_exp1
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent gmx_vmd_mcp \
+    --config benchmark/configs/gmx_vmd_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp2
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent gmx_vmd_mcp_agent \
+    --config benchmark/configs/gmx_vmd_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --test-results test_results/molecular_vis/gmx_vmd_mcp_claude-sonnet-4-5_exp2 \
+    --output eval_reports_claude-opus-4-6/molecular_vis/gmx_vmd_mcp_claude-sonnet-4-5_exp2 \
+    --agent-mode gmx_vmd_mcp_claude-sonnet-4-5_exp2
+
+python -m benchmark.evaluation_framework.run_evaluation \
+    --agent gmx_vmd_mcp \
+    --config benchmark/configs/gmx_vmd_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp3
+
+python -m benchmark.evaluation_reporter.run_reporter \
+    --agent gmx_vmd_mcp_agent \
+    --config benchmark/configs/gmx_vmd_mcp/config_anthropic.json \
+    --yaml benchmark/eval_cases/molecular_vis/eval_analysis_all.yaml \
+    --cases SciVisAgentBench-tasks/molecular_vis \
+    --test-results test_results/molecular_vis/gmx_vmd_mcp_claude-sonnet-4-5_exp3 \
+    --output eval_reports_claude-opus-4-6/molecular_vis/gmx_vmd_mcp_claude-sonnet-4-5_exp3 \
+    --agent-mode gmx_vmd_mcp_claude-sonnet-4-5_exp3
