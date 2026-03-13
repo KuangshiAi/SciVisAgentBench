4db826fba769b335318eed96c06c6457d0a0ef02
KuangshiAi <554823301@qq.com>
2026-03-13 13:16:02 -0400

    temporarily solve the saving path issue for bioimage and molecular vis

--- exp_bioimage.sh
+++ exp_bioimage.sh
@@ -3,8 +3,8 @@ python -m benchmark.evaluation_framework.run_evaluation \
     --config benchmark/configs/napari_mcp/config_openai.json \
     --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
     --cases SciVisAgentBench-tasks/bioimage_data \
-    --eval-model gpt-5.2 \
-    --experiment-number exp3
+    --eval-model claude-opus-4-6 \
+    --experiment-number exp1
 
 python -m benchmark.evaluation_reporter.run_reporter \
     --agent napari_mcp \
@@ -12,6 +12,6 @@ python -m benchmark.evaluation_reporter.run_reporter \
     --yaml benchmark/eval_cases/napari/eval_visualization_tasks.yaml \
     --cases SciVisAgentBench-tasks/bioimage_data/eval_visualization_tasks \
     --test-results test_results/bioimage_data/napari_mcp_gpt-5.2_exp3 \
-    --output eval_reports/bioimage/napari_mcp_gpt-5.2_exp3 \
+    --output eval_reports_claude-opus-4-6/bioimage/napari_mcp_gpt-5.2_exp3 \
     --agent-mode napari_mcp_gpt-5.2_exp3 \
     --port 8081
